f4e410fe3111edf028d9fea1d68e4a0743182156
João Victor <joaovictorramalho7@gmail.com>
2021-02-01 19:55:46 -0300

    QuickSort

---
+++ algoritmos_ordenacao/quickSort/quicksort.c
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+
+void swap(int* a, int* b);
+int partition(int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
+
+int main() { 
+    int arr[6] = {100, 99, 80, 70, 60, 80};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n-1);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; 
+    int i = low -1; // posição do possível menor elemento
+
+    for (int j = low; j <= high -1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i +1], &arr[high]);
+
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int indexPartition = partition(arr, low, high);
+
+        quickSort(arr, low, indexPartition -1); // depois indexPartion
+        quickSort(arr, indexPartition +1, high); // antes indexPartion
+    }
+}
+
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
