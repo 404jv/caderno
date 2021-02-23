ed474854c92afadaa52dca57f97bb9650a892568
João Victor <joaovictorramalho7@gmail.com>
2021-02-23 14:52:55 -0300

    quickSort

--- algoritmos_ordenacao/quickSort/quicksort.c
+++ algoritmos_ordenacao/quickSort/quicksort.c
@@ -1,47 +1,46 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 void swap(int* a, int* b);
-int partition(int arr[], int low, int high);
-void quickSort(int arr[], int low, int high);
+void quickSort(int lista[], int esquerda, int direita);
+int particao(int lista[], int esquerda, int direita);
 
-int main() { 
-    int arr[6] = {100, 99, 80, 70, 60, 80};
+int main(void) {
+    int lista[6] = {100, 99, 80, 70, 60, 80};
+  
+    int tamanho = sizeof(lista) / sizeof(lista[0]);
+    quickSort(lista, 0, tamanho -1);
 
-    int n = sizeof(arr) / sizeof(arr[0]);
-    quickSort(arr, 0, n-1);
-
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", lista[i]);
     }
 }
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; 
-    int i = low -1;
+int particao(int lista[], int esquerda, int direita) {
+  int pivot = lista[direita];
+  int i = esquerda -1;
 
-    for (int j = low; j <= high -1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
+  for (int j = esquerda; j <= direita -1; j++) {
+    if (lista[j] < pivot) {
+      i++;
+      swap(&lista[i], &lista[j]);
     }
+  }
 
-    swap(&arr[i +1], &arr[high]);
-    return (i + 1);
+  swap(&lista[i+1], &lista[direita]);
+  return (i + 1);
 }
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int indexPartition = partition(arr, low, high);
+void quickSort(int lista[], int esquerda, int direita) {
+  if (esquerda >= direita) return;
 
-        quickSort(arr, low, indexPartition -1); // depois indexPartion
-        quickSort(arr, indexPartition +1, high); // antes indexPartion
-    }
+  int meio = particao(lista, esquerda, direita);
+
+  quickSort(lista, esquerda, meio -1);
+  quickSort(lista, meio +1, direita);
 }
-
 
 void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
