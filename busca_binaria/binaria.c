f14244c5bb4b0f7d1c1b54f89c7a872e5ce373cd
João Victor <joaovictorramalho7@gmail.com>
2021-03-01 16:21:42 -0300

    Busca binária

--- busca_binaria/binaria.c
+++ busca_binaria/binaria.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+int buscaBinaria(int lista[], int esquerda, int direita, int elemento);
+
+int main(void) {
+  int lista[] = { 3, 4, 6, 8, 10 };
+  int tamanho = sizeof(lista) / sizeof(lista[0]); 
+  int elemento = 11;
+
+  printf("%d", buscaBinaria(lista, 0, tamanho - 1, elemento));
+}
+
 int buscaBinaria(int lista[], int esquerda, int direita, int elemento) {
   if (direita < esquerda) return -1;
  
-  int meio = esquerda + (direita - esquerda) / 2;
+  int meio = (direita + esquerda) / 2; // 10
 
   if (elemento == lista[meio]) return meio;
 
@@ -11,12 +21,4 @@ int buscaBinaria(int lista[], int esquerda, int direita, int elemento) {
     return buscaBinaria(lista, esquerda,  meio -1, elemento);
 
   return buscaBinaria(lista, meio +1, direita, elemento);
-}
-
-int main(void) {
-  int lista[] = { 2, 3, 4, 10, 40 };
-  int tamanho = sizeof(lista) / sizeof(lista[0]); 
-  int elemento = 5; 
-
-  printf("%d", buscaBinaria(lista, 0, tamanho - 1, elemento)); 
 }
