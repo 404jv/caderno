5ddfb48fa2eca4dabfb2797a3fd4cbf0bb6b96be
João Victor <joaovictorramalho7@gmail.com>
2021-03-15 17:44:03 -0300

    cpp

---
+++ exercicios_cpp/ambiente.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+  int a, b;
+  string x;
+
+  cin >> a >> b >> x;
+
+  cout << a << " " << b << " " << x;
+
+  // while (cin >> x) {}
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+
+  
+}
+
+// cin usado para input
+// cout usado para output
