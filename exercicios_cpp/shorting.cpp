9afbfb9f4fbfca61ec077233aef6fd6c4c6261ca
João Victor <joaovictorramalho7@gmail.com>
2021-03-16 15:51:42 -0300

    cpp

---
+++ exercicios_cpp/shorting.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// type name (typdef) usado para definir o nome de um tipo para outro 
+// geralmente usado para diminuir o nome.
+typedef long long ll;
+typedef vector<int> vi;
+
+// Macros (define) Outra forma de fazer isso é usando macro.
+#define F first
+#define S second
+
+int main(void) {
+  ll minhLL;
+  vi array[9];
+
+  //  cin >> minhLL;
+  //  cout << minhLL;
+
+  array[1] = 4;
+
+  cout << array[1];
+}
