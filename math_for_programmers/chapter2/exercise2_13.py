6e6fa33d896760d0e611562dfe8531fbffa25618
João Victor <joaovictorramalho7@gmail.com>
2021-04-28 22:27:01 -0300

    What are the components and lengths of the vectors (-6, -6) and (5, -12)?

---
+++ math_for_programmers/chapter2/exercise2_13.py
@@ -0,0 +1,10 @@
+from draw_vectors import *
+# What are the components and lengths of the vectors (-6, -6) and (5, -12)? 
+
+draw (
+  Points((-6, -6), (5, -12)),
+  Segment((0, 0), (-6, -6), color=purple),
+  Segment((0, 0), (5, -12), color=blue),
+
+  Segment((-6, -6),  (5, -12))
+)
