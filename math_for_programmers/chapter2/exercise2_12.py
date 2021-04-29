9151969d89b4fbb2426c2cfdb27aceec2707805c
João Victor <joaovictorramalho7@gmail.com>
2021-04-28 22:26:34 -0300

    Which is longer, the x or y component of (3, -2) + (1, 1) + (-2, -2)?

---
+++ math_for_programmers/chapter2/exercise2_12.py
@@ -0,0 +1,14 @@
+from draw_vectors import *
+
+#Which is longer, the x or y component of (3, -2) + (1, 1) + (-2, -2)?
+
+# (3, -2) + (1, 1) + (-2, -2) = (2, -3)
+
+# x = (2, 0) < y = (0, -3)
+
+#
+
+
+draw (
+  Points((3 + 1 + -2, -2 + 1 + -2))
+)
