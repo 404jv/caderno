c848e5988d62b6db0a12d8a00ffe7d6f1bdd7803
João Victor <joaovictorramalho7@gmail.com>
2021-07-12 17:27:36 -0300

    Parâmetros

--- nodejs/jornada01/fundamentos-node/src/index.js
+++ nodejs/jornada01/fundamentos-node/src/index.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello, World!.' });
+app.use(express.json());
+
+app.get('/courses', (req, res) => {
+  const { page, filtro } = req.query;
+
+  return res.json({ message: `A página é ${page} e o filtro ${filtro}`});
+});
+
+app.put('/courses/:id', (req, res) => {
+  return res.json({ message: 'Updated' });
+});
+
+
+app.post('/courses', (req, res) => {
+  const { name, age } = req.body;
+
+  return res.json({ message: `O nome é ${name} e a idade ${age}`});
 });
 
 app.listen(
