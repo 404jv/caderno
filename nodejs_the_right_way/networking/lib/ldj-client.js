825beb5c9ebc2591819b3ca0d7ea6d57be09f4a7
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-06-18 14:52:43 +0000

    challenge solved

--- nodejs_the_right_way/networking/lib/ldj-client.js
+++ nodejs_the_right_way/networking/lib/ldj-client.js
@@ -1,22 +1,43 @@
 'use strict';
 const EventEmitter = require('events').EventEmitter;
+
 class LDJClient extends EventEmitter {
   constructor(stream) {
+    if (!stream) {
+      throw new Error('LDJClient should not be null');
+    }
     super();
     let buffer = '';
+    let fullMessage = '';
     stream.on('data', data => {
       buffer += data;
+      fullMessage += data;
       let boundary = buffer.indexOf('\n');
       while (boundary !== -1) {
         const input = buffer.substring(0, boundary);
         buffer = buffer.substring(boundary + 1);
-        this.emit('message', JSON.parse(input));
+        const inputJson = this.#convertToJsonOrThrow(input)
+        this.emit('message', inputJson);
         boundary = buffer.indexOf('\n');
+      }
+    });
+
+    stream.on('end', () => {
+      if (!fullMessage.endsWith('\n')) {
+        throw new Error('Data does not end with a newline');
       }
     });
   }
   static connect(stream) {
     return new LDJClient(stream);
+  }
+
+  #convertToJsonOrThrow(data) {
+    try {
+      return JSON.parse(data)
+    } catch (error) {
+      throw new Error('data must be a JSON')
+    }
   }
 }
 
--- nodejs_the_right_way/networking/test/ldj-client-test.js
+++ nodejs_the_right_way/networking/test/ldj-client-test.js
@@ -18,5 +18,58 @@ describe('LDJClient', () => {
       done();
     });
     stream.emit('data', '{"foo":"bar"}\n');
-  })
+    stream.emit('end');
+  });
+
+  it('should emit a message event from split data events', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, { foo: 'bar' });
+    });
+    stream.emit('data', '{"foo":');
+    process.nextTick(() => {
+      stream.emit('data', '"bar"}\n');
+      stream.emit('end');
+    });
+    done();
+  });
+
+  it('should split the message over two or more data', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, { foo: 'bar' });
+    });
+    stream.emit('data', '{"foo":');
+    process.nextTick(() => {
+      stream.emit('data', '"bar"');
+      process.nextTick(() => {
+        stream.emit('data', '}\n');
+        stream.emit('end');
+      });
+    });
+    done();
+  });
+
+  it('should throw error when LDJClient constructor is null', done => {
+    assert.throws(() => {
+      new LDJClient(null);
+    }, new Error('LDJClient should not be null'));
+    done();
+  });
+
+  it('should throw if the data is not a properly formatted JSON', done => {
+    assert.throws(() => {
+      stream.emit('data', '{"foo":}\n');
+      stream.emit('end');
+    }, new Error('data must be a JSON'));
+    done();
+  });
+
+  it('should throw if the data does not have a new line', done => {
+    assert.throws(() => {
+      const data = '{"foo":';
+      stream.emit('data', data);
+      stream.emit('data', '"bar"}');
+      stream.emit('end');
+    }, new Error('Data does not end with a newline'));
+    done();
+  });
 })
